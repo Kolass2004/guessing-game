79285f3e9e06b834d45026f6ae5091321c2d1f1f
Kolass rexon J <89210243+Kolass2004@users.noreply.github.com>
2024-04-23 08:57:19 +0530

    refacter the code

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 import random
-secret_number = random.randint(1, 100)
-attempts = 0
 
-print("Welcome to the Number Guessing Game!")
-print("I'm thinking of a number between 1 and 100.")
-
-while True:
+def guess_number():
    
-    guess = int(input("Guess the number: "))
-    attempts += 1
+    secret_number = random.randint(1, 100)
+    attempts = 0
     
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100.")
+    
+    while True:  
+        guess = int(input("Guess the number: "))
+        attempts += 1
+        if guess == secret_number:
+            print(f"Congratulations! You guessed the number in {attempts} attempts.")
+            break
+        elif guess < secret_number:
+            print("Too low! Try again.")
+        else:
+            print("Too high! Try again.")
 
-    if guess == secret_number:
-        print(f"Congratulations! You guessed the number in {attempts} attempts.")
-        break
-    elif guess < secret_number:
-        print("Too low! Try again.")
-    else:
-        print("Too high! Try again.")
+# Run the game
+guess_number()
